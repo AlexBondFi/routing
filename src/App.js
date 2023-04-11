f3bc98c8e4855d388bb8289290b17b6aa5bcde9e
Alex Bond <s2300120@edu.bc.fi>
2023-04-11 12:33:51 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Routes, Route, NavLink, Link } from 'react-router-dom';
 
-function App() {
+const Home = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>This is the Home component</h1>
     </div>
   );
+};
+const About = () => {
+  return (
+    <div>
+      <h1>This is the About component</h1>
+    </div>
+  );
+};
+const Contacts = () => {
+  return (
+    <div>
+      <h1>This is the Contact component</h1>
+    </div>
+  );
+};
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to='/'>Home</NavLink>
+              </li>
+              <li>
+                <NavLink to='/about'>About</NavLink>
+              </li>
+              <li>
+                <NavLink to='/contacts'>Contacts</NavLink>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/contacts' element={<Contacts />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
